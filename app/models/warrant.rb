57eb199b64d30b363b5735816de5a83c9a392d0f
Ademir Ferreira <ademir.ae86@gmail.com>
2020-04-11 12:05:43 -0300

    Add validations

--- app/models/warrant.rb
+++ app/models/warrant.rb
@@ -2,4 +2,7 @@
 
 class Warrant < ApplicationRecord
   validates :warrant_date, :book, :first_page, :last_page, :control_number, presence: true
+  validates :book, numericality: { only_integer: true }
+  validates :first_page, numericality: { only_integer: true, greater_than: 2, less_than: 399 }
+  validates :last_page, numericality: { only_integer: true, greater_than: :first_page, less_than: 399 }
 end
