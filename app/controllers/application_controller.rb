7de3e86b275376da059057bc4e5bbe941a57fd21
Ademir Ferreira <whoisademir@gmail.com>
2020-07-12 13:09:42 -0300

    Add http basic authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  # before_action :authenticate
+  before_action :authenticate
   include Pagy::Backend
 
   private
 
-  # def authenticate
-  #   authenticate_or_request_with_http_basic do |username, password|
-  #     username == Rails.application.credentials.authentication[:username] &&
-  #       password == Rails.application.credentials.authentication[:password]
-  #   end
-  # end
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == Rails.application.credentials.authentication[:username] &&
+        password == Rails.application.credentials.authentication[:password]
+    end
+  end
 end
